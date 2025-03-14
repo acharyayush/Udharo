f23e2322ef7988c28301974ffb3074bcb54797c1
acharyayush <acharyayush.official@gmail.com>
2025-03-14 17:20:28 +0545

    Update accessing db url from env

--- server/config/db.js
+++ server/config/db.js
@@ -2,12 +2,11 @@ import mongoose from "mongoose";
 import config from "config";
 
 // mongo db compass connection string :
-const db = "mongodb://127.0.0.1:27017/udharos";
 // const db = config.get("mongoURI");
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(db);
+    await mongoose.connect(process.env.DB_URL);
     console.log("Connected to mongodb ");
   } catch (error) {
     console.error(error.message);
--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ if (!fs.existsSync("./uploads")) {
 
 app.use(
   cors({
-    origin: process.env.CLIENT_BASE_URL || "http://localhost:3000",
+    origin: process.env.CLIENT_BASE_URL,
     credentials: true,
   })
 );
