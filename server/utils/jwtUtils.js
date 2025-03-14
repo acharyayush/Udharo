c3d851387f616381af0c8e48c42ca3f033c76299
acharyayush <acharyayush.official@gmail.com>
2025-03-14 23:23:05 +0545

    Change same site to None for production and Lax for development

--- server/utils/jwtUtils.js
+++ server/utils/jwtUtils.js
@@ -1,5 +1,5 @@
-import jwt from "jsonwebtoken"
-import {ACCESS_TOKEN, REFRESH_TOKEN} from "../constants/index.js"
+import jwt from "jsonwebtoken";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants/index.js";
 export const generateJWT = (payload, type) => {
   if (type === REFRESH_TOKEN)
     return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
@@ -12,23 +12,23 @@ export const generateJWT = (payload, type) => {
   throw new Error("Unspecified token type");
 };
 export const setTokensInCookies = (res, accessToken, refreshToken) => {
-    try {
-      if(accessToken)
-        res.cookie("access_token", accessToken, {
-          httpOnly: true,
-          maxAge: 2 * 60 * 60 * 1000, //2 hr for cookie to expire but 5m for token to expire (as we have access to refresh token map)
-          secure: process.env.NODE_ENV === "production",
-          sameSite: "None",
-        });
-      if(refreshToken)
-        res.cookie("refresh_token", refreshToken, {
+  try {
+    if (accessToken)
+      res.cookie("access_token", accessToken, {
+        httpOnly: true,
+        maxAge: 2 * 60 * 60 * 1000, //2 hr for cookie to expire but 5m for token to expire (as we have access to refresh token map)
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+      });
+    if (refreshToken)
+      res.cookie("refresh_token", refreshToken, {
         httpOnly: true,
         maxAge: 2 * 60 * 60 * 1000, //2 hrs
         secure: process.env.NODE_ENV === "production",
-        sameSite: "None",
-        path: "/auth/refresh"
+        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+        path: "/auth/refresh",
       });
-    } catch (err) {
-      throw err;
-    }
-  };
+  } catch (err) {
+    throw err;
+  }
+};
